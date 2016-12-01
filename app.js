7211f4a58f0af209b7bf12b9c43b017764b0ac20
Pat Putnam <prp2986@rit.edu>
2016-12-01 09:02:35 -0500

    Some cleanup in app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('hbs');
 
@@ -28,10 +27,10 @@ var blocks = {};
 hbs.registerHelper('extend', function(name, context) {
     var block = blocks[name];
     if (!block) {
-        block = blocks[name] = [];
+            block = blocks[name] = [];
     }
 
-    block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
+    block.push(context.fn(this));
 });
 
 hbs.registerHelper('block', function(name) {
@@ -49,24 +48,23 @@ hbs.registerHelper('block', function(name) {
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true,
+    sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-    store: new redis({ logErrors: true }),
-    secret: '4DFUOJtN9b8tz9hmRjfT',
-    name: 'sessionId',
-    secure: 'true',
-    httpOnly: 'true',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 60000 }
+        store: new redis({ logErrors: true }),
+        secret: '4DFUOJtN9b8tz9hmRjfT',
+        name: 'sessionId',
+        secure: true,
+        httpOnly: true,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 60000 }
 }));
 
 app.use('/', index);
@@ -75,20 +73,21 @@ app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+
 });
 
 module.exports = app;
