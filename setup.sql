b01b20c80a219068775a3adbdb7e36643f347d54
Pat Putnam <prp2986@rit.edu>
2016-12-11 21:52:57 -0500

    Update databse setup

--- setup.sql
+++ setup.sql
@@ -1,20 +1,38 @@
-CREATE EXTENSION citext;
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA public;
+
+CREATE EXTENSION CITEXT;
 
 CREATE TABLE users (
-    id SERIAL NOT NULL,
+    id SERIAL PRIMARY KEY,
     email CITEXT NOT NULL,
     password VARCHAR(60) NOT NULL,
-    CONSTRAINT users_pk PRIMARY KEY (id),
-    CONSTRAINT users_email_key UNIQUE (email)
-)
-
-CREATE TABLE diagram (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL
+    username CITEXT NOT NULL,
+    CONSTRAINT users_email_key UNIQUE (email),
+    CONSTRAINT users_username_key UNIQUE (username)
 );
 
-CREATE TABLE user_diagram (
-    user_id INTEGER NOT NULL,
-    diagram_id INTEGER NOT NULL,
+CREATE TABLE diagrams (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    diagram TEXT
+);
+
+CREATE TABLE user_diagrams (
+    user_id INTEGER NOT NULL REFERENCES users (id),
+    diagram_id INTEGER NOT NULL REFERENCES diagrams (id),
     CONSTRAINT user_diagram_pk PRIMARY KEY (user_id, diagram_id)
 );
+
+CREATE TABLE chat_logs (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users (id),
+    diagram_id INTEGER REFERENCES diagrams (id),
+    message_datetime TIMESTAMP NOT NULL,
+    message TEXT NOT NULL
+);
+
+
+-- Add entry for lobby
+INSERT INTO diagrams VALUES
+(-1, 'Lobby', null);
