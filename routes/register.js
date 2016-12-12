27c4a5798a434295d8630a001b10a1b209dd36c6
Pat Putnam <prp2986@rit.edu>
2016-12-11 21:55:00 -0500

    Pass in username on registration

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 var bcrypt = require('bcryptjs');
-var db = require('db'); 
+var db = require('db');
 
 var router = express.Router();
 var parser = bodyParser.urlencoded({ extended: false });
@@ -13,11 +13,11 @@ router.get('/', function(req, res, next) {
 
 router.post('/', parser, function(req, res, next) {
     bcrypt.hash(req.body.password, 10, function(err, hash) {
-        db('users').insert({ email: req.body.email, password: hash }).then(function(data) {
+        db('users').insert({ email: req.body.email, password: hash, username: req.body.username }).then(function(data) {
             console.log(data);
 
             req.session.email = req.body.email;
-            res.redirect('/lobby', 200);
+            res.redirect('/lobby');
         }).catch(function(err) {
             next(err);
         });
