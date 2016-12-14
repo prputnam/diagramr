74adcdb4724b3289412292415d73aa89ecac47d3
Pat Putnam <prp2986@rit.edu>
2016-12-13 23:02:09 -0500

    Rudimentary drawing on diagrams

--- public/javascripts/diagram.js
+++ public/javascripts/diagram.js
@@ -1,7 +1,82 @@
-var canvas = new fabric.Canvas('c');
-var grid = 50;
+var
+canvas = new fabric.Canvas('c'),
+grid = 10,
+started = false,
+x = 0,
+y = 0;
 
-for (var i = 0; i < (600 / grid); i++) {
-  canvas.add(new fabric.Line([ i * grid, 0, i * grid, 600], { stroke: '#ccc', selectable: false }));
-  canvas.add(new fabric.Line([ 0, i * grid, 600, i * grid], { stroke: '#ccc', selectable: false }))
+
+for (var i = 0; i < (1000 / grid + 1); i++) {
+    canvas.add(new fabric.Line([ i * grid, 0, i * grid, 1000], { stroke: '#ccc', selectable: false }));
+    canvas.add(new fabric.Line([ 0, i * grid, 1000, i * grid], { stroke: '#ccc', selectable: false }));
 }
+
+
+
+canvas.on('mouse:down', function(options) {
+    if(canvas.getActiveObject()){
+    return false;
+    }
+
+    console.log(options);
+
+    started = true;
+    x = options.e.layerX;
+    y = options.e.layerY;
+
+    var square = new fabric.Rect({
+      width: 0,
+      height: 0,
+      left: x,
+      top: y,
+      fill: '#000'
+    });
+
+    canvas.add(square);
+    canvas.setActiveObject(square);
+});
+
+canvas.on('mouse:move', function(options) {
+    if(!started) {
+        return false;
+    }
+
+
+
+    var
+    dropX = options.e.layerX,
+    dropY = options.e.layerY,
+    w = Math.abs(dropX - x),
+    h = Math.abs(dropY - y);
+
+    if (!w || !h) {
+        return false;
+    }
+
+    var square = canvas.getActiveObject();
+
+    if(dropX > x) {
+        square.set('width', w);
+    } else {
+        square.set('left', dropX);
+        square.set('width', w);
+    }
+
+    if(dropY > y) {
+        square.set('height', h);
+    } else {
+        square.set('top', dropY);
+        square.set('height', h);
+    }
+    square.set('width', w).set('height', h);
+});
+
+canvas.on('mouse:up', function(options) {
+    if(started) {
+      started = false;
+    }
+
+    var square = canvas.getActiveObject();
+
+    canvas.add(square);
+});
