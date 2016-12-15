5f97b736af259097bf3d60df109ff9ef86abcf24
Pat Putnam <prp2986@rit.edu>
2016-12-15 09:12:43 -0500

    Correct routing issue

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     if(req.session && req.session.username) {
         res.redirect('/lobby');
-    } else {
-        res.render('login', {title: 'Login', body: '<p>Login page</p>'});
     }
 });
 
